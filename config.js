9ce998059ffda610bfd78a07a98d89c902041d15
achaladulsha20 <116875167+achaladulsha20@users.noreply.github.com>
2023-05-19 15:56:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['947714222553', 'ACHALA', true],
+  ['+94768716348', 'ACHALA', true], 
   [''] 
 ] //Number of owners
 
